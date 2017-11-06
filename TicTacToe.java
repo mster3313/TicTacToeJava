5aba15e6080d583421475e9da6f5017e5e7c77db
Mahendra Bandara <mahen6513@gmail.com>
2017-11-07 00:15:40 +0530

    Checking Pulling and Pushing

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,6 @@
 public class TicTacToe {
 
     public static void main(String[] args) {
-        System.out.println("Hello This is TicTacToe");
   controller gameConsole = new controller();
  
     }
